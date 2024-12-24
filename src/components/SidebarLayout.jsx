7d99fd7ccc8ad5d00c48123705e93f4d08e7a3da
Tari Puspita Sari <taripuspitasari12@gmail.com>
2024-12-24 11:46:34 +0700

    changed font

--- src/components/SidebarLayout.jsx
+++ src/components/SidebarLayout.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import {Outlet, Link} from "react-router-dom";
-import {useStateContext} from "../contexts/ContextProvider";
-import axiosClient from "../axios-client";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../app/slices/authSlice";
 
 export default function SidebarLayout() {
-  const {user, setToken, setUser} = useStateContext();
-
-  const onLogout = e => {
-    e.preventDefault();
-
-    axiosClient.post("/logout").then(() => {
-      setUser({});
-      setToken(null);
-    });
-  };
+  const {user} = useSelector(state => state.auth);
+  const dispatch = useDispatch();
 
   return (
     <div className="flex h-screen w-full bg-[#F5F5DC]">
@@ -24,23 +16,23 @@ export default function SidebarLayout() {
               <div className="border-b border-slate-400 flex flex-col md:flex-row gap-4 pb-2 items-center">
                 <div className="border border-black h-36 w-36 md:h-14 md:w-14 rounded-full"></div>
                 <div className="text-center md:text-left">
-                  <h3 className="font-bold">{user.name}</h3>
+                  <h3 className="font-medium">{user.name}</h3>
                   <p className="text-sm text-gray-500">{user.email}</p>
                 </div>
               </div>
             </li>
-            <li className="cursor-pointer font-bold hover:bg-[#dedec4] p-1">
+            <li className="cursor-pointer font-medium hover:bg-[#dedec4] p-1">
               Transaction
             </li>
-            <li className="cursor-pointer font-bold hover:bg-[#dedec4] p-1">
+            <li className="cursor-pointer font-medium hover:bg-[#dedec4] p-1">
               Whislist
             </li>
-            <li className="cursor-pointer font-bold hover:bg-[#dedec4] p-1">
+            <li className="cursor-pointer font-medium hover:bg-[#dedec4] p-1">
               <Link to="/account">Account</Link>
             </li>
             <li
-              className="cursor-pointer font-bold hover:bg-[#dedec4] p-1"
-              onClick={onLogout}
+              className="cursor-pointer font-medium hover:bg-[#dedec4] p-1"
+              onClick={() => dispatch(logout())}
             >
               Logout
             </li>
